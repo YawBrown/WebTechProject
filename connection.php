dc9955f744d975d59df08365847b9833397da828
YawBrown <83142991+YawBrown@users.noreply.github.com>
2021-12-10 02:52:14 +0000

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php 
+ 
+session_start(); 
+ 
+$servername="localhost"; 
+$username="root"; 
+$dbpassword=""; 
+$dbname = "COMPANY_DATABASE"; 
+
+$connection=mysqli_connect($servername,$username,$dbpassword,$dbname); 
+ 
+
+ ?>
+
+ 
